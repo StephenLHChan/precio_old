dfff842957fa3ec060486f404b73b6722549288d
StephenLHChan <stephenc.1018@gmail.com>
2023-07-01 23:17:46 -0400

    feat(db): add Product model

---
+++ server/db/models/Product.ts
@@ -0,0 +1,25 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../sequelize';
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  barcode: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
+
+export default Product;
