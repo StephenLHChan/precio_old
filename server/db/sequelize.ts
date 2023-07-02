c420a7cef5dc6a0a1342fbd9c4bb0290c6b34d02
StephenLHChan <stephenc.1018@gmail.com>
2023-07-01 22:57:17 -0400

    feat(db): create connection to database

---
+++ server/db/sequelize.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/precio',
+  {
+    logging: false,
+  }
+);
+
+export default sequelize;
