33fa6404c159ad3331e758b0f75ee102da88e7d3
StephenLHChan <stephenc.1018@gmail.com>
2023-07-02 16:55:32 -0400

    feat: add product route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import routes from './routes';
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use(routes);
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
